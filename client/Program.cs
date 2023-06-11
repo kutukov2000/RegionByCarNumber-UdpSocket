1845918d86bfccdc8e0940bbf499e9e5d4dd2584
kutukov2000 <kutukov16.2005@gmail.com>
2023-06-11 14:47:32 +0300

    Add project files.

---
+++ client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    internal class Program
+    {
+        const short port = 6666;
+        static void Main(string[] args)
+        {
+            IPEndPoint serverEndPoint=new IPEndPoint(IPAddress.Parse("192.168.3.48"), port);
+
+            UdpClient client = new UdpClient();
+
+            Console.WriteLine("Enter a message: ");
+            string message=Console.ReadLine();
+
+            byte[]data=Encoding.UTF8.GetBytes(message);
+
+            client.Send(data, data.Length, serverEndPoint);
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    internal class Program
+    {
+        const short port = 6666;
+        static void Main(string[] args)
+        {
+            UdpClient server = new UdpClient(port);
+            while (true)
+            {
+                Console.WriteLine("Wait for client reques");
+
+                IPEndPoint clientEndPoint = null;
+                byte[] requesData = server.Receive(ref clientEndPoint);
+
+                string message = Encoding.UTF8.GetString(requesData);
+
+                Console.WriteLine($"Request message: {message} from: {clientEndPoint}");
+            }
+        }
+    }
+}
